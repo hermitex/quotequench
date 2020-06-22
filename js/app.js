046efc0dda5ca4343365d1e7a013249c4b70ad88
Tiberius <mercytiberius@yahoo.com>
2020-06-22 17:29:25 +0300

    fix js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 const time = document.querySelector('.time');
+const greeting = document.querySelector('.greet #yellow');
+
+const greet = time => {
+    parseInt(time);
+    if (time >= 0 && time <= 12) {
+        greeting.innerHTML = 'Morning!';
+    } else if (time >= 12 && time <= 16) {
+        greeting.innerHTML = 'Afternoon!';
+    } else {
+        greeting.innerHTML = 'Evening!';
+    }
+};
 
 const showTime = () => {
     const today = new Date();
     const hour = today.getHours();
     const minutes = today.getMinutes();
     const seconds = today.getSeconds();
-    time.innerHTML = `<h1>${hour} : ${minutes} : <span style="color: green;">${seconds}</span></h1>`;
+    greet(hour);
+    time.innerHTML = `<h1>${isLessThanTen(hour)} : ${isLessThanTen(minutes)} : <span style="color: green;">${isLessThanTen(seconds)}</span></h1>`;
     setTimeout(() => showTime(), 1000)
 };
 
-
+const isLessThanTen = num => `${parseInt(num) < 10 ? '0' + num : num}`;
 
 time.onload = showTime();
